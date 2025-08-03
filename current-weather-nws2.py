c6dcb53ccbc3be67549b6bda47d37b107fbd002a
James Carr <jfcarr@gmail.com>
2025-08-03 15:12:36 -0400

    trim seconds from sunrise and sunset times

--- current-weather-nws2.py
+++ current-weather-nws2.py
@@ -144,6 +144,12 @@ class NWSManager:
             data_object_sr_sr = json.loads(response_sr_ss.text)
             sunrise = data_object_sr_sr['results']['sunrise']
             sunset = data_object_sr_sr['results']['sunset']
+            if sunrise.count(':') > 1:
+                last_colon_index = sunrise.rfind(':')
+                sunrise = sunrise[:last_colon_index] + sunrise[last_colon_index + 3:]
+            if sunset.count(':') > 1:
+                last_colon_index = sunset.rfind(':')
+                sunset = sunset[:last_colon_index] + sunset[last_colon_index + 3:]
 
         if sunrise is not None and sunset is not None:
             print(f"{self.leading_spaces}sunrise at {sunrise}, sunset at {sunset}")
